fe896d7105d4eebb3364bdaa5e346a10f342f72d
Aaron Sinn <aaron.sinn@me.com>
2023-06-06 17:29:59 -0400

    Finished day 3 :)

--- Day3/day3.cpp
+++ Day3/day3.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <cctype>
+#include <sstream>
 
 void parseStrings(std::string &left, std::string &right, std::string input);
 char findSameType(std::string &left, std::string &right);
@@ -17,15 +18,52 @@ int main(){
     string rightSide;
     char sameType;
     int sum = 0;
+    
+    //part two variables
+    int elfGroupSum = 0;
+    int loopCount = 0;
+    stringstream ss;
+    string firstString;
+    string secondString;
+    string thirdString;
+    char elfGroupLetter; 
+
 
     while(getline(inputFile, input)){
         parseStrings(leftSide, rightSide, input);
         sameType = findSameType(leftSide, rightSide);
         sum += typeToInt(sameType);
-        cout << sum << endl;
+
+        ss << input;
+        ss << ' ';
+        loopCount++;
+
+        if(loopCount == 3){
+            loopCount = 0;
+            ss >> firstString;
+            ss >> secondString;
+            ss >> thirdString;
+
+            cout << firstString << " " << secondString << " " << thirdString << "\n";
+            
+            for(int i = 0; i < firstString.length(); i++){
+                for(int j = 0; j < secondString.length(); j++){
+                    for(int k = 0; k < thirdString.length(); k++){
+                        if(thirdString[k] == secondString[j] && thirdString[k] == firstString[i]){
+                            elfGroupLetter = thirdString[k];
+                            cout << "letter is " << elfGroupLetter << "\n";
+                        }
+                    }
+                }
+            }
+            elfGroupSum += typeToInt(elfGroupLetter);
+        }
     }
 
     cout << "The sum of the same types is " << sum << ".\n";
+    cout << "The sum of the group letters is " << elfGroupSum << ".\n";
+
+
 
     return 0;
 }
